c76d4b4cd82b6eb98fb846d1aa03020bf3c51bd9
tawseef <tawseefalam1@gmail.com>
2024-07-09 22:21:24 +0530

    first commit

---
+++ src/controller/user.controller.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+
+const {
+  findingUserInDB,
+  gettingDatafromAPI,
+} = require("../service/user.service");
+const httpStatus = require("http-status");
+
+async function handleGetRequest(req, res) {
+  const { username } = req.params;
+  try {
+    const findingUser = await findingUserInDB(username);
+    if (findingUser) res.status(httpStatus.OK).json(findingUser);
+    else if (findingUser == false) {
+      const callingAPI = await gettingDatafromAPI(username);
+      res.status(httpStatus.CREATED).json(callingAPI);
+    }
+  } catch (error) {
+    res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ message: "error", error });
+  }
+}
+
+module.exports = { handleGetRequest };
